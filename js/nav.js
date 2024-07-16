e73f7e93172562d87ed445946c357ba3ec3bb183
Martingmdev <martingm.dev@gmail.com>
2024-07-16 17:49:48 -0600

    responsive

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     // Obtener la URL actual
-    var path = window.location.pathname;
-    var page = path.split("/").pop(); // Obtener el nombre del archivo actual
+    let path = window.location.pathname;
+    let page = path.split("/").pop(); // Obtener el nombre del archivo actual
 
     // Iterar a través de los enlaces y aplicar la clase 'active' según la URL
     $('.navbar-nav .nav-link').each(function() {
-        var href = $(this).attr('href');
+        let href = $(this).attr('href');
         // Comparar solo el nombre del archivo (por ejemplo, 'index.php' o 'modelos.php')
         if (href === page) {
             $(this).addClass('active');
